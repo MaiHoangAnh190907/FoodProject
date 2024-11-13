3f2bf3141181a21e6245e0c175d6dc098c55f807
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-11-13 23:15:03 +0700

    update/final-project

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
-  slidesPerView: 4,
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+const swiper_icon = new Swiper(".swiper-icon", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 6,
   spaceBetween: 30,
   loop: true,
 
